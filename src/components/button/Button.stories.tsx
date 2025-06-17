60cc2ad2bb511d4ff346b1061c2fe999bf2649f7
“ <adam.morsi@yahoo.com>
2025-06-17 12:33:58 +0300

    upgrade pnpm to v9.0.11

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Button } from './Button';
 
 //👇 This default export determines where your story goes in the story list
